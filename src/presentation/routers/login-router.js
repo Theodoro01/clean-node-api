80a26f1d690183ba515c2d9676058edda303c980
Theodoro01 <thedorovieira01@gmail.com>
2022-07-19 13:29:44 -0300

    modify HttpResponse.badRequest to receive an error

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -22,7 +22,7 @@ module.exports = class LoginRouter {
       }
       return HttpResponse.ok({ accessToken })
     } catch (error) {
-      console.log(error.message = null)
+      console.log(error.message = undefined)
       return HttpResponse.serverError()
     }
   }
